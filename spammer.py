e66d31444c7c326699b95af4318085e46531aca8
kristsalemy772-jpg <kristsalemy772@gmail.com>
2026-06-30 03:22:34 +0800

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -20,7 +20,14 @@ For inquiries and availment, send a direct message ~ <a href="https://t.me/phia_
 No Free 🫪 GETS!?</i></blockquote>"""
 
 # 2. TEMPLATE PARA SA CODM
-CODM_MESSAGE = """<a href="https://t.me/KAZELIDERMODS/380">𝘊𝘖𝘋𝘔 𝘐𝘕𝘑𝘌𝘊𝘛𝘖𝘙 𝘛𝘙𝘐𝘈𝘓 𝘊𝘓𝘖𝘚𝘌!!!...</a>"""
+CODM_MESSAGE = """<blockquote expandable="true"><i>Latest Update:
+Call of Duty v1.6.56
+Injector v2.7.4
+
+NEED KEY LOGIN ??? :
+For inquiries and availment, send a direct message ~ <a href="https://t.me/phia_maganda">𝑷𝒉𝒊𝒂 𝑭𝒆𝒍𝒊𝒄𝒊𝒂</a>
+
+<a href="https://slider-info-slidermod-register-deviceid-3g7j.onrender.com/free">Try Free Key Here:</a></i></blockquote>"""
 
 # 3. TEMPLATE PARA SA PROMO (Naka-Quote + Malinis na Italic Fonts)
 PROMO_MESSAGE = """<blockquote>𝘗𝘙𝘖𝘔𝘖 &lt; 30𝘋𝘈𝘠𝘚 &gt; (𝘔𝘓𝘉𝘉 𝘝𝘪𝘗)
@@ -54,7 +61,7 @@ async def loop_spam():
             print(f"Message sent! (Index: {index} | ID: {sent_message.message_id})")
 
             # 2. BIBILANG NG 2 MINUTO (120 SECONDS) BAGO BURAHIN
-            await asyncio.sleep(600)
+            await asyncio.sleep(10)
 
             # 3. BURAHIN PAGKATAPOS NG 2 MINUTO
             await bot.delete_message(
